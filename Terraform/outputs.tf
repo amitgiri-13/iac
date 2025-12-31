9e24e0b23d8bf7031c8c0822c882a6b457f9302c
amit <amit101giri@gmail.com>
2025-12-31 21:20:12 +0545

    Provisioned ec2 and seure s3 with custom module 	modified:   main.tf 	modified:   outputs.tf 	modified:   variables.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,14 +1,14 @@
-output "data_bucket" {
-  description = "Secure primary S3 bucket"
-  value       = aws_s3_bucket.secure_data.id
-}
-
-output "logs_bucket" {
-  description = "S3 bucket storing access logs"
-  value       = aws_s3_bucket.access_logs.id
-}
-
 output "ec2_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.lab_ec2.public_ip
+  value       = module.ec2.public_ip
+}
+
+output "log_bucket_id" {
+  description = "Access logs bucket name"
+  value       = module.access_logs_bucket.bucket_id
+}
+
+output "data_bucket_id" {
+  description = "Data bucket name"
+  value       = module.data_bucket.data_bucket_id
 }
